626b2c3eab4dccb3501525254533e3ffa2457b29
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-08-29 15:24:32 +0200

    Faker library installed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Travel;
+use Faker\Generator as Faker;
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index(Faker $faker) {
 
         
         return view('home');
